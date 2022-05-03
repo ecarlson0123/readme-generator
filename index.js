2d99fbf71fd438de04f1b45f02758f571ee1204d
MrVoze <ecarlson0123@yahoo.com>
2022-05-03 00:08:06 -0500

    final testing

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = ['What is your project title? (Required)','Please enter a description of your project. (Required)','Please enter installation instructions for your project.',
-'Please enter usage instructions.', 'Please enter contribution guidelines.','Please enter testing guidelines.', 'Please select licenses to be applied to your README.'];
+const questions = ['What is your project title? (Required)',
+'Please enter a description of your project. (Required)',
+'Please enter installation instructions for your project.',
+'Please enter usage instructions.',
+'Please enter contribution guidelines.',
+'Please enter testing guidelines.',
+'Please select licenses to be applied to your README.'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/'+fileName + '.md', data, err => {
+      // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+      if (err) {
+        reject(err);
+        // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+        return;
+      }
+
+      // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    });
+  });
+};
+
 
 // TODO: Create a function to initialize app
 function init() {
@@ -143,13 +167,34 @@ function init() {
               }
         },
         {
-            type: 'checkbox',
+            type: 'list',
             name: 'licenses',
             message: questions[6],
-            choices: ['Test', 'Test 1', 'Test 2']
+            choices: ['The Do What the Fuck You Want to Public License', 'IBM Public License Version 1.0', 'MIT License', 'No License']
         },
+        {
+          type: 'input',
+          name: 'fileName',
+          message: 'Please enter a name for your README file.',
+          validate: contributionInput => {
+            if (contributionInput) {
+              return true;
+            } else {
+              console.log('Please enter a name for your file!');
+              return false;
+            }
+          }
+      }
       ]);
 }
 
+
+
 // Function call to initialize app
-init();
+init()
+    .then(readmeData => {
+        return [ readmeData.fileName, generateMarkdown(readmeData)]
+    })
+    .then((readmeText) => {
+        writeToFile(readmeText[0],readmeText[1])
+    });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,111 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license==='The Do What the Fuck You Want to Public License'){
+    return '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)]'
+  }
+  else if(license === 'IBM Public License Version 1.0'){
+    return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)]'
+  }
+  else if(license === 'MIT License'){
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'The Do What the Fuck You Want to Public License'){
+    return '(http://www.wtfpl.net/about/)'
+  }
+  else if(license === 'IBM Public License Version 1.0'){
+    return '(https://opensource.org/licenses/IPL-1.0)'
+  }
+  else if(license === 'MIT License'){
+    return '(https://opensource.org/licenses/MIT)'
+  }
+  return
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+
+
+// GENERATE SECTIONS
+const generateLicense = license => {
+  if (license === 'No License') {
+    return '';
+  }
+  return `
+  ## **License**
+  ${license}
+  `;
+}
+
+const generateInstallation = installationText => {
+  if (!installationText) {
+    return '';
+  }
+
+  return `
+  ## **Installation**
+  ${installationText}
+  `;
+}
+
+const generateUsage = usageText => {
+  if (!usageText) {
+    return '';
+  }
+
+  return `
+  ## **Usage**
+  ${usageText}
+  `;
+}
+
+const generateContribution = contributionText => {
+  if (!contributionText) {
+    return '';
+  }
+
+  return `
+  ## **Contribution Guidelines**
+  ${contributionText}
+  `;
+}
+
+const generateTesting = testingText => {
+  if (!testingText) {
+    return '';
+  }
+
+  return `
+  ## **Testing Guidelines**
+  ${testingText}
+  `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const { title, description, installation, usage, contribution, testing, licenses, fileName} = data
+  return readmeText=`
+  ${renderLicenseBadge(licenses)}${renderLicenseLink(licenses)}
+  # ${title}
+  ## **Description**
+  ${description}
 
-`;
+  ${generateInstallation(installation)}
+
+  ${generateUsage(usage)}
+
+  ${generateContribution(contribution)}
+  
+  ${generateTesting(testing)}
+
+  ${generateLicense(licenses)}
+  `;
 }
 
 module.exports = generateMarkdown;
